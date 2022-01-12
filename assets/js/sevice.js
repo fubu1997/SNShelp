0d2e9810a0ad77d09226c7cf573416863b035e7c
fubu1997 <fubu1997@naver.com>
2022-01-13 01:30:37 +0900

    SNShelp : 07

--- assets/js/sevice.js
+++ assets/js/sevice.js
@@ -1,87 +1,91 @@
-$(".instarSevice").click(function () {
-  $(".instarSeviceDescription").toggle();
-  $(".youtubeSeviceDescription").hide();
-  $(".tiktokSeviceDescription").hide();
-  $(".facebookSeviceDescription").hide();
-  $(".twitterSeviceDescription").hide();
-  $(".pinterestSeviceDescription").hide();
-  $(".TelegramSeviceDescription").hide();
-  $(".webSeviceDescription").hide();
-});
+$("#serviceImg img").click(function () {
+  $(".서비스목록Content").toggle();
 
-$(".youtubeSevice").click(function () {
-  $(".instarSeviceDescription").hide();
-  $(".youtubeSeviceDescription").toggle();
-  $(".tiktokSeviceDescription").hide();
-  $(".facebookSeviceDescription").hide();
-  $(".twitterSeviceDescription").hide();
-  $(".pinterestSeviceDescription").hide();
-  $(".TelegramSeviceDescription").hide();
-  $(".webSeviceDescription").hide();
-});
+  $(".instarSevice").click(function () {
+    $(".instarSeviceDescription").toggle();
+    $(".youtubeSeviceDescription").hide();
+    $(".tiktokSeviceDescription").hide();
+    $(".facebookSeviceDescription").hide();
+    $(".twitterSeviceDescription").hide();
+    $(".pinterestSeviceDescription").hide();
+    $(".TelegramSeviceDescription").hide();
+    $(".webSeviceDescription").hide();
+  });
 
-$(".tiktokSevice").click(function () {
-  $(".instarSeviceDescription").hide();
-  $(".youtubeSeviceDescription").hide();
-  $(".tiktokSeviceDescription").toggle();
-  $(".facebookSeviceDescription").hide();
-  $(".twitterSeviceDescription").hide();
-  $(".pinterestSeviceDescription").hide();
-  $(".TelegramSeviceDescription").hide();
-  $(".webSeviceDescription").hide();
-});
+  $(".youtubeSevice").click(function () {
+    $(".instarSeviceDescription").hide();
+    $(".youtubeSeviceDescription").toggle();
+    $(".tiktokSeviceDescription").hide();
+    $(".facebookSeviceDescription").hide();
+    $(".twitterSeviceDescription").hide();
+    $(".pinterestSeviceDescription").hide();
+    $(".TelegramSeviceDescription").hide();
+    $(".webSeviceDescription").hide();
+  });
 
-$(".facebookSevice").click(function () {
-  $(".instarSeviceDescription").hide();
-  $(".youtubeSeviceDescription").hide();
-  $(".tiktokSeviceDescription").hide();
-  $(".facebookSeviceDescription").toggle();
-  $(".twitterSeviceDescription").hide();
-  $(".pinterestSeviceDescription").hide();
-  $(".TelegramSeviceDescription").hide();
-  $(".webSeviceDescription").hide();
-});
+  $(".tiktokSevice").click(function () {
+    $(".instarSeviceDescription").hide();
+    $(".youtubeSeviceDescription").hide();
+    $(".tiktokSeviceDescription").toggle();
+    $(".facebookSeviceDescription").hide();
+    $(".twitterSeviceDescription").hide();
+    $(".pinterestSeviceDescription").hide();
+    $(".TelegramSeviceDescription").hide();
+    $(".webSeviceDescription").hide();
+  });
 
-$(".twitterSevice").click(function () {
-  $(".instarSeviceDescription").hide();
-  $(".youtubeSeviceDescription").hide();
-  $(".tiktokSeviceDescription").hide();
-  $(".facebookSeviceDescription").hide();
-  $(".twitterSeviceDescription").toggle();
-  $(".pinterestSeviceDescription").hide();
-  $(".TelegramSeviceDescription").hide();
-  $(".webSeviceDescription").hide();
-});
+  $(".facebookSevice").click(function () {
+    $(".instarSeviceDescription").hide();
+    $(".youtubeSeviceDescription").hide();
+    $(".tiktokSeviceDescription").hide();
+    $(".facebookSeviceDescription").toggle();
+    $(".twitterSeviceDescription").hide();
+    $(".pinterestSeviceDescription").hide();
+    $(".TelegramSeviceDescription").hide();
+    $(".webSeviceDescription").hide();
+  });
 
-$(".pinterestSevice").click(function () {
-  $(".instarSeviceDescription").hide();
-  $(".youtubeSeviceDescription").hide();
-  $(".tiktokSeviceDescription").hide();
-  $(".facebookSeviceDescription").hide();
-  $(".twitterSeviceDescription").hide();
-  $(".pinterestSeviceDescription").toggle();
-  $(".TelegramSeviceDescription").hide();
-  $(".webSeviceDescription").hide();
-});
+  $(".twitterSevice").click(function () {
+    $(".instarSeviceDescription").hide();
+    $(".youtubeSeviceDescription").hide();
+    $(".tiktokSeviceDescription").hide();
+    $(".facebookSeviceDescription").hide();
+    $(".twitterSeviceDescription").toggle();
+    $(".pinterestSeviceDescription").hide();
+    $(".TelegramSeviceDescription").hide();
+    $(".webSeviceDescription").hide();
+  });
 
-$(".telegramSevice").click(function () {
-  $(".instarSeviceDescription").hide();
-  $(".youtubeSeviceDescription").hide();
-  $(".tiktokSeviceDescription").hide();
-  $(".facebookSeviceDescription").hide();
-  $(".twitterSeviceDescription").hide();
-  $(".pinterestSeviceDescription").hide();
-  $(".TelegramSeviceDescription").toggle();
-  $(".webSeviceDescription").hide();
-});
+  $(".pinterestSevice").click(function () {
+    $(".instarSeviceDescription").hide();
+    $(".youtubeSeviceDescription").hide();
+    $(".tiktokSeviceDescription").hide();
+    $(".facebookSeviceDescription").hide();
+    $(".twitterSeviceDescription").hide();
+    $(".pinterestSeviceDescription").toggle();
+    $(".TelegramSeviceDescription").hide();
+    $(".webSeviceDescription").hide();
+  });
 
-$(".webSevice").click(function () {
-  $(".instarSeviceDescription").hide();
-  $(".youtubeSeviceDescription").hide();
-  $(".tiktokSeviceDescription").hide();
-  $(".facebookSeviceDescription").hide();
-  $(".twitterSeviceDescription").hide();
-  $(".pinterestSeviceDescription").hide();
-  $(".TelegramSeviceDescription").hide();
-  $(".webSeviceDescription").toggle();
+  $(".telegramSevice").click(function () {
+    $(".instarSeviceDescription").hide();
+    $(".youtubeSeviceDescription").hide();
+    $(".tiktokSeviceDescription").hide();
+    $(".facebookSeviceDescription").hide();
+    $(".twitterSeviceDescription").hide();
+    $(".pinterestSeviceDescription").hide();
+    $(".TelegramSeviceDescription").toggle();
+    $(".webSeviceDescription").hide();
+  });
+
+  $(".webSevice").click(function () {
+    $(".instarSeviceDescription").hide();
+    $(".youtubeSeviceDescription").hide();
+    $(".tiktokSeviceDescription").hide();
+    $(".facebookSeviceDescription").hide();
+    $(".twitterSeviceDescription").hide();
+    $(".pinterestSeviceDescription").hide();
+    $(".TelegramSeviceDescription").hide();
+    $(".webSeviceDescription").toggle();
+  });
 });
